70428d4a42c64a43d929c1973c3cb61f8ef1a97e
Jeeseob <jeeseob5761@gmail.com>
2022-03-25 15:11:01 +0900

    냄새 1 : 이해하기 힘든 이름

---
+++ refactoring/src/main/java/me/whiteship/refactoring/_02_duplicated_code/_01_before/StudyDashboard.java
@@ -0,0 +1,50 @@
+package me.whiteship.refactoring._02_duplicated_code._01_before;
+
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class StudyDashboard {
+
+    private void printParticipants(int eventId) throws IOException {
+        GHIssue issue = getGhIssue(eventId);
+        Set<String> participants = getUserNames(issue);
+        print(participants);
+    }
+
+    private void printReviewers() throws IOException {
+        GHIssue issue = getGhIssue(30);
+        Set<String> reviewers = getUserNames(issue);
+        print(reviewers);
+    }
+
+    // 코드를 블럭화하고, cmd+option+M을 누르면 함수로 만들어준다.
+    private GHIssue getGhIssue(int eventId) throws IOException {
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        return repository.getIssue(eventId);
+    }
+
+    private Set<String> getUserNames(GHIssue issue) throws IOException {
+        Set<String> userNames  = new HashSet<>();
+        issue.getComments().forEach(c -> userNames.add(c.getUserName()));
+        return userNames;
+    }
+
+    private void print(Set<String> participants) {
+        participants.forEach(System.out::println);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        StudyDashboard studyDashboard = new StudyDashboard();
+        studyDashboard.printReviewers();
+        studyDashboard.printParticipants(15);
+
+    }
+
+}
